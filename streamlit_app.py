c70a50af0cd4e2aacbbc39d8ab57630ffdf93f23
Rohit Rangaraj <rrohit@vt.edu>
2023-08-15 18:02:55 -0400

    Fixed auth

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from google.cloud import firestore
 from google.oauth2 import service_account
 import json
+# import firebase_admin
+# from firebase_admin import firestore
 
 
 def clear_form():
@@ -11,7 +13,10 @@ def clear_form():
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="streamlit-reddit")
+db = firestore.Client(credentials=creds, project="streamlit-reddit-7290e")
+
+
+# db = firestore.Client.from_service_account_json("firestore-key.json")
 
 title = st.text_input("Post title", key="title_input")
 url = st.text_input("Post URL", key="url_input")
