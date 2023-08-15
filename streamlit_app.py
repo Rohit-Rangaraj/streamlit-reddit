030e4fc7aed16399290709ce754a03ca2a459d58
Rohit Rangaraj <rrohit@vt.edu>
2023-08-15 18:21:01 -0400

    Minor edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 from google.cloud import firestore
 from google.oauth2 import service_account
 import json
-# import firebase_admin
-# from firebase_admin import firestore
 
 
 def clear_form():
@@ -11,16 +9,19 @@ def clear_form():
     st.session_state["url_input"] = ""
 
 
+st.set_page_config("Reddit Clone", layout="wide",
+                   page_icon="https://www.redditstatic.com/shreddit/assets/favicon/120x120.png")
+
+st.header("Reddit Clone")
+
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="streamlit-reddit-7290e")
 
-
-# db = firestore.Client.from_service_account_json("firestore-key.json")
-
 title = st.text_input("Post title", key="title_input")
 url = st.text_input("Post URL", key="url_input")
 submit_btn = st.button("Submit!")
+clear_btn = st.button("Clear form", on_click=clear_form)
 
 if title and url and submit_btn:
     doc_ref = db.collection("posts").document(title)
